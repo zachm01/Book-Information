5c445a556e5538daea9f4838973f800aba79f0ab
zachm01 <62446698+zachm01@users.noreply.github.com>
2023-03-16 08:56:42 -0400

    Add functionality to main.py

--- bookdata/main.py
+++ bookdata/main.py
@@ -4,24 +4,15 @@ import pprint
 from title_to_url import search
 from bookdata import BookData
 
-def summary(search_term: str) -> dict:
+def summary(search_term: str, fpath="") -> dict:
     """Get information about a book"""
     b = BookData(search(search_term))
-    return b.summary()
+    if fpath:
+        return b.summary()
+    else:
+        return b.summary(filepath=fpath)
 
 if __name__ == "__main__":
     # Nicely render the dict
     pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(summary(input("Book? ")))
-
-# if you would like to have the option to export the summary as a JSON, uncomment the following code:
-#
-# def summary(search_term: str, fpath: str) -> dict:
-#     """Get book information and export"""
-#     b = BookData(search(search_term))
-#     summary = b.summary(filepath=fpath)
-#     return summary
-# if __name__ == "__main__":
-#     Nicely render the dict
-#     pp = pprint.PrettyPrinter(indent=2)
-#     pp.pprint(summary(input("Book? "), input("Filepath? ")))
+    pp.pprint(summary(input("Book? "), input("Export filepath? (Leave blank if none)")))
