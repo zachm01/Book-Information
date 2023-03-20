c69e36cc9fe50ef21cd6d20d706b4bbd88fb606c
zachm01 <62446698+zachm01@users.noreply.github.com>
2023-03-20 11:26:49 -0400

    Update bookdata.py

--- bookdata/bookdata.py
+++ bookdata/bookdata.py
@@ -26,18 +26,18 @@ class BookData():
     Get data about books from GoodReads using web scraping
 
     Available methods:
-     -- get_title(): returns a book\'s title
-     -- get_author(): returns a book\'s author
-     -- get_synopsis(): returns a plot summary of the book
-     -- get_rating(): returns a book\'s average reader rating
-     -- get_genres(): returns a list of the genres this book has ben labeled as
-     -- get_year(): returns the year the book was published
-     -- get_page_ct(): returns the page count of the book
-     -- get_image(): returns a URL where an image of the cover of the book can
+     -- ``get_title()``: returns a book\'s title
+     -- ``get_author()``: returns a book\'s author
+     -- ``get_synopsis(): returns a plot summary of the book
+     -- ``get_rating()``: returns a book\'s average reader rating
+     -- ``get_genres()``: returns a list of the genres this book has ben labeled as
+     -- ``get_year()``: returns the year the book was published
+     -- ``get_page_ct()``: returns the page count of the book
+     -- ``get_image()``: returns a URL where an image of the cover of the book can
                      be found
-     -- download_cover(): downloads the image from the URL retrieved from 
+     -- ``download_cover()``: downloads the image from the URL retrieved from 
                           get_image()
-     -- summary(): returns a dict with the values of all the above methods, 
+     -- ``summary()``: returns a dict with the values of all the above methods, 
                    excluding download_cover() - user can choose to export to a
                    .JSON file or not
     """
@@ -45,7 +45,7 @@ class BookData():
         self.url = book_url
         try:
             # Get all the data from the page
-            self.page = requests.get(self.url)
+            self.page = requests.get(self.url, timeout=10)
             # Parse the page content
             self.soup = BeautifulSoup(self.page.content, "html.parser")
             print("200 Connection successful")
@@ -64,27 +64,27 @@ class BookData():
             title = str(self.soup.find_all("h1", class_="Text Text__title1"))
             tag = "data-testid=\"bookTitle\">"
             return title[title.index(tag)+len(tag):title.index("</h1>")].replace("&amp;", "&")
-        except Exception:
+        except:
             print("Could not fetch book title")
-            return ""
+            return "title"
 
     def get_rating(self):
         """Get the rating of a book"""
         try:
             rating = str(self.soup.find_all("div", class_="RatingStatistics__rating"))
             return float(rating[rating.index('">')+2:rating.index("</div>")])
-        except Exception:
+        except:
             print("Could not fetch average book rating")
-            return ""
+            return "rating"
 
     def get_author(self):
         """Get the author of a book"""
         try:
             author = str(self.soup.find_all("h3", class_="Text Text__title3 Text__regular"))
             return author[author.index("By: ")+4:author.index("class")-2].replace("&amp;", "&")
-        except Exception:
+        except:
             print("Could not fetch book author")
-            return ""
+            return "author"
 
     def get_synopsis(self):
         """Get a summary of a book"""
@@ -93,13 +93,16 @@ class BookData():
                                         class_="DetailsLayoutRightParagraph__widthConstrained"))
             summary = summary[summary.index("Formatted\">")+11:summary.index("</span></div>")]
             summary = self.remove_html_tags(summary.replace("<br/><br/>", "")).replace("&amp;", "&")
+
+            # Used to remove Librarian Notes
             cbfh = "can be found here"
+
             if cbfh in summary:
                 return summary[summary.index(cbfh): + len(cbfh):]
-            return summary
-        except Exception:
+            return summary.replace("'", "\'")
+        except:
             print("Could not fetch book summary")
-            return ""
+            return "synopsis"
 
     def get_genres(self):
         """Get the genres of a book"""
@@ -110,18 +113,18 @@ class BookData():
                 label_tag = "Button__labelItem\">"
                 genres[i] = genre[genre.index(label_tag)+len(label_tag):]
             return [i.replace("</span></a></span>", "") for i in genres]
-        except Exception:
+        except:
             print("Could not fetch book genres")
-            return ""
+            return "genres"
 
     def get_year(self):
         """Get the publication info of a book"""
         try:
             info = str(self.soup.find_all("div", class_="FeaturedDetails"))
             return info[info.index("First"):info.index("</p></div>")][-4:]
-        except Exception:
+        except:
             print("Could not fetch book year")
-            return ""
+            return "year"
 
     def get_page_ct(self):
         """Get the page count of a book"""
@@ -129,18 +132,18 @@ class BookData():
             info = str(self.soup.find_all("div", class_="FeaturedDetails"))
             id_str = "pagesFormat\">"
             return int(info[info.index(id_str)+len(id_str):info.index("pages, ")-1])
-        except Exception:
+        except:
             print("Could not fetch book page count")
-            return ""
+            return "page count"
 
     def get_image(self):
         """Get URL of an image of the cover of a book"""
         try:
             url = str(self.soup.find_all("img", class_="ResponsiveImage"))
-            return url[url.index("src=\"htt")+5:url.index("\"/>]")]
-        except Exception:
+            return url[url.index("src=\"htt")+5:url.index(".jpg\"/>]")] + ".jpg"
+        except:
             print("Could not fetch book cover")
-            return ""
+            return "image url"
 
     def download_cover(self, filepath="cover.jpg"):
         """Download an image of the cover of a book"""
@@ -153,13 +156,13 @@ class BookData():
                     f.close()
             except OSError:
                 print("Failed to write file")
-            except Exception:
+            except:
                 print("Error occured when writing cover data file")
 
             img = Image.open('cover.jpg')
             img.show()
 
-        except Exception:
+        except:
             print("Failed to download cover")
 
     def summary(self, filepath=""):
