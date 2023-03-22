37b9418ff7bd4854bcef9edea36d70d4b9fe34cd
Zachary Masaitis <legomaster119@gmail.com>
2023-03-22 11:24:53 -0400

    Clarified docs

--- bookdata/title_to_url.py
+++ bookdata/title_to_url.py
@@ -18,23 +18,28 @@ def search(title: str):
       output = https://www.goodreads.com/book/show/2165.The_Old_Man_and_the_Sea?from_search=true&from_srp=true&qid=GUgCAMeZjU&rank=1
     """
     title.replace(' ', '+')
-    
+
     # Scrapes search results from GoodReads
     data = requests.get(f"https://goodreads.com/search?utf8=✓&query={title}")
-    
+
     soup = BeautifulSoup(data.content, "html.parser")
     # From GoodReads HTML
     search_results = soup.find_all("div", class_="u-anchorTarget")
-    
+
     # Get the first search result
     res = str(search_results[0])
-    
+
     book_id = re.search('id="[0-9]*"', res).group(0)
-    
+
     # From GoodReads HTML
     ix = str(soup).index(str(soup.find_all("div", id=book_id[4:-1])[0]))
+
+    # this shit super jank
     html = str(soup)[ix:ix+1000]
+
+    # get the first result
     link = html[html.index("/book/show/"):html.index("rank=1\"") + len("rank=1")]
+
     url = f"https://goodreads.com{link}"
     return url
 
